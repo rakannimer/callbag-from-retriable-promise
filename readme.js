f3f25a1be4c676fe2997e3483e067288dd8eff6d
rakannimer <rakannimer@gmail.com>
2018-06-23 12:57:13 +0300

    Initial

---
+++ readme.js
@@ -0,0 +1,72 @@
+/* 
+  callbag-from-retriable-promise
+  
+  Usage : 
+  
+  pipe(fromRetriablePromise(() => fetch('https://wikipedia.org'), 4), forEach(console.log));
+
+  Copy the code you need and inline it in your project.
+
+  Or install with your favorite package manager :
+
+  pnpm i callbag-from-retriable-promise
+*/
+
+const fromRetriablePromise = (promise, desiredRetryCount = 0) => (
+  start,
+  sink
+) => {
+  let retryCount = 0;
+  if (start !== 0) return;
+  let ended = false;
+  const onfulfilled = val => {
+    if (ended) return;
+    sink(1, val);
+    sink(2);
+  };
+  const onrejected = err => {
+    if (retryCount === desiredRetryCount) {
+      if (ended) return;
+      sink(2, err);
+    } else {
+      retryCount += 1;
+      promise()
+        .then(onfulfilled)
+        .catch(onrejected);
+    }
+  };
+  promise()
+    .then(onfulfilled)
+    .catch(onrejected);
+  sink(0, t => {
+    if (t === 2) ended = true;
+  });
+};
+
+// https://github.com/staltz/callbag-for-each
+const forEach = operation => source => {
+  let talkback;
+  source(0, (t, d) => {
+    if (t === 0) talkback = d;
+    if (t === 1) operation(d);
+    if (t === 1 || t === 0) talkback(1);
+  });
+};
+
+// https://github.com/staltz/callbag-pipe
+const pipe = (...cbs) => {
+  let res = cbs[0];
+  for (let i = 1, n = cbs.length; i < n; i++) res = cbs[i](res);
+  return res;
+};
+
+const someThrowablePromise = () => {
+  return new Promise((resolve, reject) => {
+    Math.random() < 0.99 ? reject(new Error("Error")) : resolve("Ok");
+  });
+};
+
+pipe(
+  fromRetriablePromise(async () => someThrowablePromise(), 200),
+  forEach(console.log)
+);
